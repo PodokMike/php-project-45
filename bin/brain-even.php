1975d2ab05674d79d6d35cac13b9b565c736c3da
Mikhail Dolgov <podok2@gmail.com>
2024-10-21 21:59:23 +0300

    refactoring Even-Game

--- bin/brain-even.php
+++ bin/brain-even.php
@@ -3,8 +3,10 @@
 <?php
 
 require_once "src/Cli.php";
+require_once "src/Even.php";
 
 use function BrainGames\Cli\greeting;
+use function BrainGames\Even\evenGame;
 
 $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
@@ -15,4 +17,4 @@ if (file_exists($autoloadPath1)) {
     require_once $autoloadPath2;
 }
 
-greeting();
+evenGame(greeting());
--- src/Even.php
+++ src/Even.php
@@ -14,16 +14,16 @@ function evenGame(string $name = "user")
         $randNum = rand();
         line("Question: {$randNum}");
         $answer = prompt('Your answer: ');
-        if ($randNum %2 === 0 && $answer === "yes") {
+        if ($randNum % 2 === 0 && $answer === "yes") {
             line("Correct!");
             $winCount++;
-        } elseif ($randNum %2 !== 0 && $answer === "no") {
+        } elseif ($randNum % 2 !== 0 && $answer === "no") {
             line("Correct!");
             $winCount++;
-        } elseif ($randNum %2 === 0 && $answer !== "yes") {
+        } elseif ($randNum % 2 === 0 && $answer !== "yes") {
             line("'yes' is wrong answer ;(. Correct answer was 'no'.");
             line("Let's try again, {$name}!");
-        } elseif ($randNum %2 !== 0 && $answer !== "no") {
+        } elseif ($randNum % 2 !== 0 && $answer !== "no") {
             line("'yes' is wrong answer ;(. Correct answer was 'yes'.");
             line("Let's try again, {$name}!");
         }
