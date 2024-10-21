40d195659eec6caafeadfad065b99f2c52644c2a
Mikhail Dolgov <podok2@gmail.com>
2024-10-21 16:46:12 +0300

    add phpcs and fixed style

--- src/Cli.php
+++ src/Cli.php
@@ -1,11 +1,13 @@
 <?php
-   namespace BrainGames\Cli;
 
-   use function cli\line;
-   use function cli\prompt;
+namespace BrainGames\Cli;
 
-   function greeting(string $name = "user"){
+use function cli\line;
+use function cli\prompt;
+
+function greeting(string $name = "user")
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-   }
+}
