02a28069561fd0916b33100260dc89b6dd033758
Mikhail Dolgov <podok2@gmail.com>
2024-10-30 17:08:53 +0300

    Init Calc-game add bin add make

---
+++ bin/brain-calc.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+
+<?php
+
+require_once "src/Cli.php";
+require_once "src/Calc.php";
+
+use function BrainGames\Cli\greeting;
+use function BrainGames\Calc\calc;
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+calc();
---
+++ src/Calc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BrainGames\Calc;
+
+use function cli\line;
+use function cli\prompt;
+
+function calc()
+{
+    line("hello Calc");
+}
